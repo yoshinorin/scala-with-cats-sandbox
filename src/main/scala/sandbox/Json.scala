644b6773c81b47e421e0531100d171826cb9096c
yoshinorin <yoshinorin.net@outlook.com>
2022-05-25 08:33:19 +0900

    update

---
+++ src/main/scala/sandbox/Json.scala
@@ -0,0 +1,31 @@
+package sandbox
+
+// シンプルなJSON ASTを定義
+sealed trait Json
+final case class JsObject(get: Map[String, Json]) extends Json
+final case class JsString(get: String) extends Json
+final case class JsNumber(get: Double) extends Json
+final case object JsNull extends Json
+
+// JSONのシリアライズが実装されるtrait
+trait JsonWriter[A] {
+  def write(value: A): Json
+}
+
+final case class Person(name: String, email: String)
+
+object JsonWriterInstances {
+  implicit val stringWriter: JsonWriter[String] =
+    new JsonWriter[String] {
+      def write(value: String): Json = JsString(value)
+    }
+
+  implicit val personWriter: JsonWriter[Person] =
+    new JsonWriter[Person] {
+      def write(value: Person): Json =
+        JsObject(Map(
+          "name" -> JsString(value.name),
+          "email" -> JsString(value.email)
+        ))
+    }
+}
