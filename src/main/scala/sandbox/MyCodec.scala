b0227c844c091fa18f52b4fd860cd66654c06fd5
yoshinorin <yoshinorin.net@outlook.com>
2022-08-03 10:17:39 +0900

    update

---
+++ src/main/scala/sandbox/MyCodec.scala
@@ -0,0 +1,30 @@
+package sandbox
+
+object MyCodec {
+
+  trait Codec[A] {
+    def encode(value: A): String
+    def decode(value: String): A
+    def imap[B](dec: A => B, enc: B => A): Codec[B] = ???
+  }
+
+  def encode[A](value: A)(implicit c: Codec[A]): String = c.encode(value)
+
+  def decode[A](value: String)(implicit c: Codec[A]): A = c.decode(value)
+
+
+  implicit val stringCodec: Codec[String] = {
+    new Codec[String] {
+      def encode(value: String): String = value
+      def decode(value: String): String = value
+    }
+  }
+
+  implicit val intCodec: Codec[Int] =
+    stringCodec.imap(_.toInt, _.toString)
+
+  implicit val booleanCodec: Codec[Boolean] =
+    stringCodec.imap(_.toBoolean, _.toString)
+
+}
+
