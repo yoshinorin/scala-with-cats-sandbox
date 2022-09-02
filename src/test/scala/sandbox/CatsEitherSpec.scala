0e80594d77771d2efc2ee8f2f2c96f7192f10e68
yoshinorin <yoshinorin.net@outlook.com>
2022-09-03 04:43:32 +0900

    update

--- src/test/scala/sandbox/CatsEitherSpec.scala
+++ src/test/scala/sandbox/CatsEitherSpec.scala
@@ -17,4 +17,32 @@ class CatsEitherSpec extends AnyWordSpec {
     // z <- c  `c` は `Left` なので ここではつかえない
   } yield x * x + y * y
 
+  Either.catchOnly[NumberFormatException]("foo".toInt)
+  Either.catchNonFatal(sys.error("Badness"))
+  Either.fromTry(scala.util.Try("foo".toInt))
+  Either.fromOption[String, Int](None, "Badness")
+
+  -1.asRight[String].ensure("Must be non-negative!")(_ > 0)
+
+  "error".asLeft[Int].recover {
+    case _: String => -1
+  }
+
+  "error".asLeft[Int].recoverWith {
+    case _: String => Right(-1)
+  }
+
+  "foo".asLeft[Int].leftMap(_.reverse)
+  6.asRight[String].bimap(_.reverse, _ * 7)
+  "bar".asLeft[String].bimap(_.reverse, _ * 7)
+
+  123.asRight[String].swap
+  123.asLeft[String].swap
+
+  for {
+    a <- 1.asRight[String]
+    b <- 0.asRight[String]
+    c <- if (b == 0) "DIV0".asLeft[Int]
+         else (a / b).asRight[String]
+  } yield c * 100
 }
